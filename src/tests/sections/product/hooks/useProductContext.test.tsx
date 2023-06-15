1dca0814a63c80d338e6e5f55755b7184b51dc77
Osvaldo Castillo <osvaldo.castillo@isucorp.ca>
2023-06-15 08:27:56 -0400

    [test]: test setProduct and remove from cart

--- src/tests/sections/product/hooks/useProductContext.test.tsx
+++ src/tests/sections/product/hooks/useProductContext.test.tsx
@@ -54,4 +54,19 @@ describe('useProductContext', () => {
 		);
 		expect(containTheSameCategory).toBeTruthy();
 	});
+
+	it('should set a product', () => {
+		const { result } = renderCustomHook();
+		const pickedProduct = result.current.products[3];
+		act(() => result.current.setSelectedProduct(pickedProduct));
+		expect(result.current.product?.sku).toBe(pickedProduct.sku);
+	});
+
+	it('should remove a product from cart', () => {
+		const { result } = renderCustomHook();
+		const productSku = result.current.products[1].sku;
+		act(() => result.current.addToCart(productSku));
+		act(() => result.current.removeFromCart(productSku));
+		expect(result.current.cartSize).toBe(0);
+	});
 });
